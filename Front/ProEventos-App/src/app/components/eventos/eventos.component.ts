e49502e58fff35b5cbfef2e1fe6d4af73516289d
gabrielpgf <gabrielpgf2011@gmail.com>
2023-06-07 17:27:45 -0300

    Seção 7: Angular - Organizando, Rota, Alerta e +

---
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { EventoService } from '../../services/evento.service';
+import { Evento } from '../../models/Evento';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss'],
+  // providers: [EventoService]
+})
+export class EventosComponent implements OnInit {
+  modalRef?: BsModalRef;
+  public eventos: Evento[] = [];
+  public eventosFiltrados: Evento[] = [];
+  public widthImg: number = 140;
+  public marginImg: number = 2;
+  public showImage: boolean = false;
+  public showOrHide: string = 'Mostrar';
+  private filtroListado: string = '';
+
+  public get filtroLista(): string {
+    return this.filtroListado;
+  }
+
+  public set filtroLista(value: string) {
+    this.filtroListado = value;
+    this.eventosFiltrados = this.filtroListado
+      ? this.filtrarEventos(this.filtroListado)
+      : this.eventos;
+  }
+
+  public filtrarEventos(filtrarPor: string): Evento[] {
+    filtrarPor = filtrarPor.toLowerCase();
+    return this.eventos.filter(
+      (evento: any) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+  ) {}
+
+  public ngOnInit(): void {
+    /** spinner starts on init */
+    this.spinner.show();
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.eventoService.getEventos().subscribe({
+      next: (eventos: Evento[]) => {
+        this.eventos = eventos;
+        this.eventosFiltrados = this.eventos;
+      },
+      error: (error: any) => {
+        this.spinner.hide();
+        this.toastr.error('Falha ao carregar os eventos.', 'Erro!');
+      },
+      complete: () => this.spinner.hide()
+    });
+
+    //OUTRA FORMA DE FAZER
+    // const observer = {
+    //   next: (eventos: Evento[]) => {
+    //     this.eventos = eventos;
+    //     this.eventosFiltrados = this.eventos;
+    //   },
+    //   error: (error: any) => console.log(error),
+    // }
+    // this.eventoService.getEventos().subscribe(observer);
+  }
+
+  public mostrarImagem(): void {
+    this.showImage = !this.showImage;
+  }
+
+  openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
+  }
+
+  confirm(): void {
+    this.modalRef?.hide();
+    this.toastr.success('O Evento foi deletado com sucesso.', 'Deletado!');
+  }
+
+  decline(): void {
+    this.modalRef?.hide();
+  }
+}
